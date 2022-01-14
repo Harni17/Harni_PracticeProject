840bbf3413b552d8d7933c5a999d529b779276e6
Harni17 <97238928+Harni17@users.noreply.github.com>
2022-01-14 22:09:47 +0530

    Add files via upload

---
+++ calculator.java
@@ -0,0 +1,35 @@
+package com.HarniK.Practice_Project;
+
+import java.util.Scanner;
+
+public class calculator {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("enter two numbers: ");
+		int firstNo = sc.nextInt();
+		int secondNo = sc.nextInt();
+		
+		System.out.println("Enter the arithmetic operator: ");
+		char arithmetic = sc.next().charAt(0);
+		double result=0;
+
+		switch(arithmetic){
+		case '+': 
+			result=firstNo+secondNo;
+			break;
+		case '-':
+			result=firstNo-secondNo;
+			break;
+		case '*':
+			result=firstNo*secondNo;
+			break;
+		case '/':
+			result=firstNo/secondNo;
+			break;
+		}
+		System.out.println("the result is: " +result);
+	}
+
+
+}
